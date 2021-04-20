7b48aaa5070cda4fe5f337aac6c8088ac2a56c4a
Si Hyeong Lee <super2451894@gmail.com>
2021-04-20 13:58:39 +0900

    Change Dice text to Image
    - Change Dice text to Image

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+//import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,13 +20,24 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "주사위가 굴려졌습니다", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage= findViewById((R>.id))
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
 
         val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
     }
 }
